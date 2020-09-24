8fdf13a2bcf2342557758cc00052dec4896247b3
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-09-24 15:52:53 +0200

    refactor: extracting refIsValidAndExist to helpers

--- src/api/serie/controllers/create-serie.js
+++ src/api/serie/controllers/create-serie.js
@@ -1,12 +1,12 @@
 const HTTP_CODE = require('http-status-codes');
-const mongoose = require('mongoose');
 
 const { Serie, serieJoiSchema } = require('../models');
 const { Brand } = require('../../brand/models');
+const { refIsValidAndExist } = require('../../../helpers');
 
 const createSerie = async (req, res) => {
   try {
-    const { error } = serieJoiSchema.validate(req.body, { abortEarly: false });
+    let { error } = serieJoiSchema.validate(req.body, { abortEarly: false });
     if (error)
       return res.status(HTTP_CODE.BAD_REQUEST).json({
         status: HTTP_CODE.BAD_REQUEST,
@@ -14,30 +14,8 @@ const createSerie = async (req, res) => {
         error
       });
 
-    const isValidId = mongoose.Types.ObjectId.isValid(req.body.brandId);
-    if (isValidId) {
-      const brandExists = await Brand.findById(req.body.brandId);
-      if (!brandExists)
-        return res.status(HTTP_CODE.BAD_REQUEST).json({
-          status: HTTP_CODE.BAD_REQUEST,
-          mdg: "Brand doesn't exist"
-        });
-    } else
-      return res.status(HTTP_CODE.BAD_REQUEST).json({
-        status: HTTP_CODE.BAD_REQUEST,
-        mdg: "brandId isn't a valid ObjectId"
-      });
-
-    // const serieExits = await Serie.find({ brandId: req.body.brandId });
-    // if (serieExits)
-    //   serieExits.forEach(serie => {
-    //     if (serie.name === req.body.name)
-    //       return res.status(HTTP_CODE.CONFLICT).json({
-    //         status: HTTP_CODE.CONFLICT,
-    //         msg: `Serie with name (${req.body.name}) for Brand with id (${req.body.brandId}) already exists.`
-    //       });
-    //     return null;
-    //   });
+    error = await refIsValidAndExist(req.body.brandId, Brand);
+    if (error) return res.status(HTTP_CODE.BAD_REQUEST).json(error);
 
     const serie = new Serie({
       brandId: req.body.brandId,
